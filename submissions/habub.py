f2d663ae62d84b11f4454e2f12d4ea197893b1bd
Shalev Wengrowsky <shalevwen@gmail.com>
2025-12-24 15:38:24 +0200

    switched from unicode to base85

--- submissions/habub.py
+++ submissions/habub.py
@@ -1,4 +1,5 @@
 # ruff: noqa: F403, F405
+import base64
 from functools import reduce
 from itertools import *  # pyright: ignore[reportWildcardImportFromLibrary]
 from strats import *
@@ -18,32 +19,49 @@ perm_scenarios = list(
 )
 
 
-def unpack_int(s: str) -> int:
-    return int("".join(f"{ord(c):0{4 if (ord(c) > 0xFF) else 2}x}" for c in s), 16)
-
-
 personas = (Alice, Bob, Charlie, Dan)
 data_table = dict(
     (
         k + 9 * (k >= 18) + 29 * (k >= 45),
         (
-            [i for i in range(144) if unpack_int(dat) & (0b100 << i)],
-            personas[unpack_int(dat) & 0b11],
+            [i for i in range(144) if int(dat, 16) & (0b100 << i)],
+            personas[int(dat, 16) & 0b11],
         ),
     )
     for k, dat in enumerate(
-        ",𑦱㹤䕊쑄总ထ馑៤䱂,,选ꋢ툤⪰༙ﻙ㤈챧坣,𤄉䠈눴蠖딂䘌ᐉ蠀ᵠ,𢂆♣⋅你䢈静飀꡶梥,,,,ﻠ洄鈃∃  ஈ  顅,‑艂䉠  ␉餕馸謄┬,  겢썤삡ࢱ쥜ę鹡옱,妀    蘀̔  쉀뀞㈃,•  ဉ到類肀  ဣ,𑑍ࡡ䐌渣㒱脐聃ʂ䐅,𘫈ा塒  惈藐Ƥ  ⁇,弁쐩Ӓ  閺눚傂∄,䬒  ჲ  䎀膚ࢁ꠬,  ,愩ᧁ㔅Ⴢꐍ  ᅈ    ,즡    ⾪䵔၀  鋊,𢞲          禊㯼㻽,p㐽  ဃ脉谑      ,   뿙ʀ耚        ,鼿    刀  㧧,ⲡ      ꒋ    쿝ߍ,茈          菒  ﹡,궡  Ｂ    ဿ  羕,נּ    Ҁ  ࠇ  ﻰဣ,𦞤  锐髡      ꅰ倓,   큤    찳  短,hᏽ            ࠐ,㕂      ᥚ      ,ꈗ          ⛯м  ,䠒  穼  䪷    胣  ,怐҈  က䌖屰    ៦,𠋿햊        ֈ  ﹒,2  쥴쬅          鏶,搃  ᕸॅ  ೀ  כּ,𘥀  ﹙          ﹲ,널﹘  芯,𘁔  歰  世    컭  ,ↀ  蠬㩊        ➭,h  聀ͅ⾑ր  퐿,n态舢䀰⪁        ,挻﷿    㿭,,ﯽￖ        ,緇곆￢        ,,Ｏ  ﾴ奶,蟾  쑞,,,﷿    㯻    蟜,,뽭  㻾,ｷ﷿      믭,,ﭾ？        ,﷿    ﾢ㾲,,,㏸,,玢    鑡      ᗞ,忿羠              ,,ﷴ    侞        痉,ﭱ      쿽    ꥮ,,,젠됏            ꗭ,,矞            ,𢮿              ﷿,,ﱟ        ﷿,ﾯ    뫋      ,,,찵㐞        Ｙ  ﾽ,,翿  濜  ɷ혛,﷽,,쿯  ﷽,,,,ﾥ  ᾬ    롟ﾥ,,ﯯﲽ              ,ￗ먟    矿,,ꛫ        嶷  뭿,忿        왮,,,枿  櫃    豨,,c息              彺,￭ﯼ홉,,烿          ,翷  Ｍ,,,路樂    ,,䠇    ꫳ,뿿            쑇,,꺿翱            屲,敇ꌛ    ᨿ,,,𘯪              ㍟,,彿    暟,廿            ྿,,𧿲쯽            巹,ￒ尠      ၢ".split(
-            ","
-        )
+        hex(int.from_bytes(base64.b85decode('(HXHeWJOBEL|{7*5t)$}#7sinkO`vl@+2y-4;lZNIS9;WS7Xva2}lUCG>8_p0!9oJ35Wn0VAAMfW-=^P_5_HDb(xF`hh~ah-QWKP@JZt?KH#{H>4aV37$hVn4&p$Wky)FFutHMJ?yRE2WWb>avB_Kk8J}UsG1Zt3?&x3uF;46x3jy=v-XiW02~sfm@PL5s5F^nPNeE#?3~wVev4IePLjr<C1<{Jg2|rj;?qJA;@ByUm<b7WO#3=;w?v=W-8c>2RO-mB)5c2LrfPtC_fv7Cr?O`by!8HXC!lV!G5lHUt+?pQl;`*fZK?DxzEUhAYF7EE`?&p}Jzr+8Xa5O*e5Cee;j1lhc?%wXd`2v7|8t(4y?%DJH?&aX_Irq}a9`5eyNWSju|L}aBg9z^K?(Xh`^6vj(+WH>v@-FV-1ODgvUHd@p?gW7D2nX)}@DL-?XQb|x5SsDs?(U&*P!rzn@MP}p%rowH`PP9yp6>4M?(X1$4K+gU?(P{{?(Xi|A%5=e?(X7m@I%h+ND}UPeC|rO?(Tr|?$!YSNRHtE1TkgD4({Q9ZQ}p+itg_2?gfbM|5DQ~?3YZiUheMh?&&{mWCQLMcnL-B48ZRDJJE?i?*Cct?(XjH|8m<A5A}}dD*YG*PUg1`<neCq?5h6lA%O0PEILZ=?(Xg<sn(9@1OPNr`4CtR=|lZ$U;%<6KrkwS?(XjH)-%7K{_f-Yo!`Ho`EKs+?(KQUtj7PM?(XjH-TyD{|Fl_l*nh6<MXlZW|L*QP`|j?CoZY`~?mGJ4_kaKX?(XZlo!_^w@&4}a?(O;i?(YAhKeFA^KaJNqF7D|@9`5epS*>6Hf1vK}?(XjH?%nzH?(R>T?(XjHb;;ki9`5e!|DNvZmaW~$>p=eQ?(XjH>RO%Gd9Lp6?(XjGBD?<X?(XjH?(d)d-^Kp!?(XlO{r|7-?z+qF?(W^(!&5=7?(XjI^Pcbfo!9^V=K0RxCwXDL`T5<?ukQKz-QEAC?jNk~?zmt7rQQ3l{JHM#?(XjH?%(-)q5kgYfBo8OzV7bs<(q!%xBXxL?(XjH#%kTxd;aFEKJMtojn*~>{_gJX?(Xj8_pSe_`}~$k-EjZz?(XjH*Z+R+@}1rJH~GKr?%hZS?(VAd+yDOV?(XjH>_mOqtM~sN?(XjH?&ZfVWk;hM?(P~t-O-Dx?(XjH?(Xh0U)^7S?(Sxv)vx~U?(XjH;QRg3fAY)u?(XjH?(SLn-}xoZ?(XgoVg')))[2:].replace("e", "20").split("d")
     )
     if dat
 )
 for dat, k in enumerate(
-    "߭볪㼀,𘣰ꙵ흚钾ⴃ,␏뛣괆㙗,㥝᭮𮎵𝗓𢩔,Ṕ䜚묨,鳞ç𫴤,᭬ᓦ㴁,鿱懸㗊蓀딄,ᮃﴈ묉,ǘ䭫𐍬颙𢤂,Ṏ㧯㩠,ᯎ맋㜥,जƦ㤍,𘧩歹𗒪N👒,বഎ㔋,𘪻瓹㫺ꦌ뛹,ᶭ羽뜧,᷄騶딦,諊𝣚f,ᩌ綖镀,ᩬ鶚霶,ƥ좛𬼿,ڎ漈,梮𖬼𡃟5".split(
-        ","
+    (
+        8717658111744,
+        1855606874592557829860611,
+        2598496598992762455,
+        1109575324838066334200900180,
+        33347319020328,
+        10780073442596,
+        30151021051137,
+        755308995549597421057284,
+        30253699873545,
+        2230348200446401667279106,
+        33321328261728,
+        30574694315813,
+        10015891405069,
+        1860195863383588496077906,
+        10634558059787,
+        1864070364225449761748729,
+        32633062864679,
+        32730238072102,
+        9537542740582,
+        28915826857280,
+        29053802944310,
+        28984749510463,
+        109997832,
+        7542991387203002165,
     )
 ):
-    for k_i in batched((bin(unpack_int(k))[:1:-1]), 9):
+    for k_i in batched((bin(k)[:1:-1]), 9):
         data_table[int("".join(k_i[::-1]), 2)] = dat  # pyright: ignore[reportArgumentType]
 
 
